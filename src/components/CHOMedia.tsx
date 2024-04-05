1b45860bfcbf57c58396df5ccbc016f87454f78e
Shinichi Haramizu <shinichi@haramizu.jp>
2024-04-05 16:21:29 +0900

    update richtext
    add CHOMedia component
    update CodeBlock component

---
+++ src/components/CHOMedia.tsx
@@ -0,0 +1,27 @@
+import { getMediaFromID } from "@/utils/getMedia";
+import Image from "next/image";
+
+interface CHOMediaProps {
+  mediaId: string;
+  index: number;
+}
+
+export default async function CHOMedia(porps: CHOMediaProps) {
+  const media = await getMediaFromID(porps.mediaId);
+
+  return (
+    <div className="flex justify-center my-4" key={porps.index}>
+      <div className="w-4/5 shadow-xl border">
+        <Image
+          src={
+            media.data.media.fileUrl ||
+            "https://sitecorecontenthub.stylelabs.cloud/api/public/content/91c3d57209b042ff9aacfee56125ef0e"
+          }
+          height={media.data.media.fileHeight}
+          width={media.data.media.fileHeight}
+          alt="Image"
+        />
+      </div>
+    </div>
+  );
+}
